8695c2d7bbee321757160bf32d10e9cff5fb02e5
Palroset <palrosetakhar@gmail.com>
2026-02-11 14:50:25 -0700

    Update SelectionSort.java

--- src/main/java/org/example/SelectionSort.java
+++ src/main/java/org/example/SelectionSort.java
@@ -5,15 +5,30 @@ import java.util.List;
 
 public class SelectionSort {
 
-  public static <T> List<T> Sort(List<T> list) {
-    // Create a selection sort algorithm (find reference to python code in module 2)
-    // Compared based on their calculated hourly salary
-    List<T> sorted_list = new ArrayList<>();
+  public static List<Employee> Sort(List<Employee> list) {
 
+    List<Employee> sorted_list = new ArrayList<>(list);
+
+    for (int i = 0; i < sorted_list.size() - 1; i++) {
+
+      int minIndex = i;
+
+      for (int j = i + 1; j < sorted_list.size(); j++) {
+
+        if (sorted_list.get(j).CalcHourlySalary() <
+            sorted_list.get(minIndex).CalcHourlySalary()) {
+
+          minIndex = j;
+        }
+      }
+
+      Employee temp = sorted_list.get(i);
+      sorted_list.set(i, sorted_list.get(minIndex));
+      sorted_list.set(minIndex, temp);
+    }
 
     return sorted_list;
   }
-
 }
 
 
